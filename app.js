348933457ca191fc552bbcfc4e4340ab224581ae
ayberksch <35408256+Ayberkyvs@users.noreply.github.com>
2023-07-20 14:46:12 +0300

    js

--- app.js
+++ app.js
@@ -4,10 +4,18 @@ run();
 
 function run(){
     body.addEventListener("click",hideAll);
+    document.addEventListener("DOMContentLoaded",addDemo);
 }
 
+function addDemo(e){
+    setTimeout(() => {
+        demodiv.forEach(x => {
+            x.classList.remove("hidden");
+        });
+      }, 300);
+}
 let sayac = 0;
-function hideAll(){
+function hideAll(e){
     if(sayac % 2 == 0){
         demodiv.forEach(x => {
             console.log(x);
