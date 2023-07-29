c707098a4b05787cf06102b125ca7f8470b21359
ayberksch <35408256+Ayberkyvs@users.noreply.github.com>
2023-07-29 03:25:05 +0300

    force to clear cache

--- yapim/app.js
+++ yapim/app.js
@@ -2,29 +2,30 @@ const body = document.querySelector(".container-fluid");
 const demodiv = document.querySelectorAll(".demo");
 run();
 
-function run(){
-    body.addEventListener("click",hideAll);
-    document.addEventListener("DOMContentLoaded",addDemo);
+function run() {
+  body.addEventListener("click", hideAll);
+  document.addEventListener("DOMContentLoaded", addDemo);
 }
 
-function addDemo(e){
-    setTimeout(() => {
-        demodiv.forEach(x => {
-            x.classList.remove("hidden");
-        });
-      }, 300);
+function addDemo(e) {
+  window.location.reload(true);
+  setTimeout(() => {
+    demodiv.forEach((x) => {
+      x.classList.remove("hidden");
+    });
+  }, 300);
 }
 let sayac = 0;
-function hideAll(e){
-    if(sayac % 2 == 0){
-        demodiv.forEach(x => {
-            console.log(x);
-            x.classList.toggle("hidden");
-        });
-    }else{
-        demodiv.forEach(x => {
-            x.classList.remove("hidden");
-        });
-    }
-    sayac++;
+function hideAll(e) {
+  if (sayac % 2 == 0) {
+    demodiv.forEach((x) => {
+      console.log(x);
+      x.classList.toggle("hidden");
+    });
+  } else {
+    demodiv.forEach((x) => {
+      x.classList.remove("hidden");
+    });
+  }
+  sayac++;
 }
