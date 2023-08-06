fc063eaaece95aba685d2f85b765b60bec7123c8
ayberksch <35408256+Ayberkyvs@users.noreply.github.com>
2023-08-07 00:54:47 +0300

    deneme

--- php/connect.php
+++ php/connect.php
@@ -13,4 +13,26 @@ if (!$conn) {
 }
 
 echo "Connected successfully!";
+// Query to fetch data from the "products" table
+$sql = "SELECT * FROM products";
+$result = $conn->query($sql);
+
+// Check if there are any rows returned
+if ($result->num_rows > 0) {
+    // Initialize an empty array to store the data
+    $productsArray = array();
+
+    // Loop through each row and fetch the data
+    while ($row = $result->fetch_assoc()) {
+        $productsArray[] = $row;
+    }
+
+    // Now $productsArray contains all the rows as an array
+    print_r($productsArray);
+} else {
+    echo "No products found!";
+}
+
+// Close the connection
+$conn->close();
 ?>
