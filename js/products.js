065001273e8ca310a4e6265e6474e8cdbc86c213
ayberksch <35408256+Ayberkyvs@users.noreply.github.com>
2023-08-07 18:11:05 +0300

    update

--- js/products.js
+++ js/products.js
@@ -1,3 +1,4 @@
+veriCek();
 async function veriCek() {
   try {
     const response = await fetch("/products/data.json");
@@ -5,10 +6,28 @@ async function veriCek() {
 
     // JSON verilerini işleme
     // Örneğin, verileri ekrana yazdırma:
-    console.log("asdasdad");
-    console.log(data);
+    addToUI(data);
   } catch (error) {
     console.error("Veri çekme hatası:", error);
   }
 }
-veriCek();
+
+const LightList = [];
+function addToUI(data) {
+  for (let i = 0; i < data.length; i++) {
+    if (data[i].category === "light") {
+      LightList.push(data[i]);
+    }
+  }
+  addToLightSection(LightList);
+}
+function addToLightSection(data) {
+  const image = document.querySelectorAll(".light-image");
+  const title = document.querySelectorAll(".light-title");
+  const text = document.querySelectorAll(".light-text");
+  for (let i = 0; i < data.length; i++) {
+    title[i].innerHTML = data[i].title;
+    image[i].src = data[i].image;
+    text[i].innerHTML = data[i].description;
+  }
+}
